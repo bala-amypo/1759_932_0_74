f9e57e1df0dd2fbd262d369e892659f501f1d5a4
bala-amypo <bala@amypo.in>
2025-12-17 06:25:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,47 +1,52 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-public class StudentEntity{
+public class StudentEntity {
 
-    @Id  //primary key
-    @GeneratedValue(strategy=GenerationType.IDENTITY) //to generate values automatically
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private float cgpa;
     private String email;
+    private float cgpa;
 
-    public void setName(String name){
-        this.name=name;
+    public StudentEntity(Long id, String name, String email, float cgpa) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.cgpa = cgpa;
     }
-    public String getName(){
+
+    public StudentEntity() {
+        
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
         return this.name;
     }
-    public void setCgpa(float cgpa){
-        this.cgpa=cgpa;
+
+    public void setEmail(String email) {
+        this.email = email;
     }
-    public float getCgpa(){
-        return this.cgpa;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public String getEmail(){
+
+    public String getEmail() {
         return this.email;
     }
-    public StudentEntity(Long id,String name,float cgpa,String email){
-        this.id=id;
-        this.name=name;
-        this.cgpa=cgpa;
-        this.email=email;
+
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
     }
 
-    public StudentEntity(){
-
+    public float getCgpa() {
+        return this.cgpa;
     }
-   
 }
